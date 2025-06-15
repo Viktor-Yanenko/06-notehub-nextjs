4f00642adfb0f27414d144ad20ea4b76c027d65c
Viktor <viktoryanenko11@gmail.com>
2025-06-15 15:31:25 +0300

    create start folder, create Header and Footer components, add main page, add api.ts

---
+++ src/types/note.ts
@@ -0,0 +1,14 @@
+export type NoteTag = 'Todo' | 'Work' | 'Personal' | 'Meeting' | 'Shopping';
+
+export interface Note {
+    id: number;
+    title: string;
+    content: string;
+    tag: NoteTag;
+}
+
+export interface NewNoteData{
+    title: string;
+    content: string;
+    tag: NoteTag
+}
